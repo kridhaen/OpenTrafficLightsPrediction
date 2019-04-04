8fc687191f1874cdbbe02b3362c8de4505b1f6bb
kridhaen <Kurt.Dhaene@UGent.be>
2019-04-04 11:04:50 +0200

    update to new distribution and package n3

--- data_reader.js
+++ data_reader.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const n3 = require('n3');
 const FrequencyDistribution = require("./Prediction/FrequencyDistribution.js");
 const TimeFrequencyDistribution = require('./Prediction/TimeFrequencyDistribution.js');
+const TimeGroupedFrequencyDistribution = require('./Prediction/TimeGroupedFrequencyDistribution.js');
+
 
 const { DataFactory } = n3;
 const { namedNode, literal, defaultGraph, quad } = DataFactory;
@@ -9,8 +11,6 @@ const { namedNode, literal, defaultGraph, quad } = DataFactory;
 
 class FragmentConverter{
     constructor(){
-        this.lastPreviousUrl = null;
-        this.lastLatest = "0";
         this.readAndParseSync = this.readAndParseSync.bind(this);
         this.parseAndStoreQuads = this.parseAndStoreQuads.bind(this);
     }
@@ -18,7 +18,7 @@ class FragmentConverter{
     parseAndStoreQuads(_doc) {
         return new Promise(resolve => {
             const parser = new n3.Parser();
-            const store = n3.Store();
+            const store = new n3.Store();
             parser.parse(_doc, (error, quad, prefixes) => {
                 if (quad)
                     store.addQuad(quad);
@@ -35,6 +35,7 @@ class FragmentConverter{
         let lastPhase = {}; //om de laatst tegengekomen fase op te slaan, voor iedere observatie
         let timeFrequencyDistribution = new TimeFrequencyDistribution();
         let frequencyDistribution = new FrequencyDistribution();
+        let timeGroupedFrequencyDistribution = new TimeGroupedFrequencyDistribution;
 
         let temp = this;
         //console.log("\x1b[31m","reading","\x1b[0m");
@@ -99,9 +100,11 @@ class FragmentConverter{
                                     frequencyDistribution.add(signalGroup, lastPhase[signalGroup], Math.round(phaseDuration/1000));
 
                                     //opslaan in frequentieverdeling op tijdstippen gesplitst
-                                    let temp = Math.floor(observationUTC["minute"]/20)*20;
                                     timeFrequencyDistribution.add(signalGroup, lastPhase[signalGroup], observationUTC["year"], observationUTC["month"], observationUTC["day"], observationUTC["hour"], Math.floor(observationUTC["minute"]/20)*20, Math.round(phaseDuration/1000));
-                                    
+
+                                    //opslaan in frequentietabel gesplitst op week of weekend en op uur
+                                    timeGroupedFrequencyDistribution.add(signalGroup, lastPhase[signalGroup], observationUTC["day"]===0||6 ? 1 : 0, observationUTC["hour"], Math.round(phaseDuration/1000));
+
                                     //klaarzetten voor volgende fase
                                     lastPhase[signalGroup] = signalPhase;
                                     phaseStart[signalGroup] = generatedAtTime;
@@ -122,11 +125,9 @@ class FragmentConverter{
                 });
             }
             console.log("\x1b[31m","read complete","\x1b[0m");
-            let a = frequencyDistribution.createDistributionsCSV();
-            let b = timeFrequencyDistribution.createDistributionsCSV();
-            console.log(a + b);
-            FragmentConverter.printToFile(timeFrequencyDistribution.createDistributionsCSV(),"./csv_data/time_csv_data_",".csv");
             FragmentConverter.printToFile(frequencyDistribution.createDistributionsCSV(),"./csv_data/csv_data_",".csv");
+            FragmentConverter.printToFile(timeGroupedFrequencyDistribution.createDistributionsCSV(),"./time_grouped_csv_data/csv_data_",".csv");
+            FragmentConverter.printToFileSync(timeFrequencyDistribution.createDistributionsCSV(),"./time_csv_data/time_csv_data_",".csv");
         }); 
     }
 
@@ -145,6 +146,18 @@ class FragmentConverter{
                 if(err) throw err;
                 console.log('File saved!');
             });
+        }
+    }
+
+    static printToFileSync(data, filename, extension){
+        if(Array.isArray(data)){
+            let count = 0;
+            data.forEach((item) => {
+                fs.writeFileSync(filename + count++ + extension, item,'utf8');
+            });
+        }
+        else{
+            fs.writeFile(filename, data,'utf8');
         }
     }
 
