2c9ad3bb7974375b55e04d402500fcf9134e8f50
Kurt Normaal <Kurt.Dhaene@UGent.be>
2019-04-11 15:43:49 +0200

    static methods in separate class

---
+++ src/Readers/Downloader.js
@@ -0,0 +1,52 @@
+class Downloader{
+    static download(_url){
+        return new Promise((resolve,reject) => {
+
+            https.get(_url, (resp) => {
+                let data = '';
+
+                // A chunk of data has been recieved.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                    resolve(data);
+                });
+            }).on("error", (err) => {
+                console.log("\x1b[31m\x1b[47m",err,"\x1b[0m"); reject(err);
+                reject(err);
+            });
+
+        });
+    }
+
+    static download2(_url){
+        console.log("\x1b[32m","downloading: "+_url,"\x1b[0m");
+        //const caAgent = new https.Agent({ca: rootca});
+        return new Promise((resolve,reject) => {
+
+            fetch(_url, { timeout: 2000000 })
+                .then(function(response) {
+                    resolve(response.text());
+                })
+                .catch(err => {console.log("\x1b[31m\x1b[47m",err,"\x1b[0m"); reject(err)});
+        });
+    }
+
+    static download1(_url){
+        console.log("\x1b[32m","downloading: "+_url,"\x1b[0m");
+        const httpAgent = new https.Agent({ rejectUnauthorized: false });
+        return new Promise((resolve,reject) => {
+
+            axios.get(_url, {httpAgent})
+                .then(function(response) {
+                    resolve(response.data);
+                })
+                .catch(err => {console.log("\x1b[31m\x1b[47m",err,"\x1b[0m"); reject(err)});
+        });
+    }
+}
+
+module.exports = Downloader;
