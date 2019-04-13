7de2442b8f683eb004d66438fe6752f7c532ab92
kridhaen <Kurt.Dhaene@UGent.be>
2019-04-13 14:20:28 +0200

    use manager in main

--- src/main.js
+++ src/main.js
@@ -3,19 +3,10 @@ const HistoricFileSystemReader = require('./Readers/HistoricFileSystemReader.js'
 const RealTimeReader = require('./Readers/RealTimeReader.js');
 const PredictionPublisher = require('./Publisher/PredictionPublisher.js');
 const FragmentParser = require('./Readers/FragmentParser.js');
-const FrequencyDistribution = require("./Distributions/FrequencyDistribution.js");
-const TimeFrequencyDistribution = require('./Distributions/TimeFrequencyDistribution.js');
-const TimeGroupedFrequencyDistribution = require('./Distributions/TimeGroupedFrequencyDistribution.js');
+const DistributionManager = require('./Distributions/DistributionManager.js');
 
 let distributionStore = new DistributionStore();
-
-let timeFrequencyDistribution = new TimeFrequencyDistribution();
-let frequencyDistribution = new FrequencyDistribution();
-let timeGroupedFrequencyDistribution = new TimeGroupedFrequencyDistribution();
-
-distributionStore.add(frequencyDistribution, "fd");
-distributionStore.add(timeFrequencyDistribution, "tfd");
-distributionStore.add(timeGroupedFrequencyDistribution, "tgfd");
+DistributionManager.createDistributions(distributionStore);
 
 let historicFragmentParser = new FragmentParser();
 let historicFileSystemReader = new HistoricFileSystemReader(historicFragmentParser, distributionStore);
@@ -30,7 +21,4 @@ historicFileSystemReader.readAndParseSync()
         let realTimeReader = new RealTimeReader(distributionStore, predictionPublisher, realTimeFragmentParser);
         realTimeReader.getLatestCyclic(6000);
 
-
-        // let predictionPublisher = new PredictionPublisher(8080);
-        // predictionPublisher.setLatestEndpoint();
     });
