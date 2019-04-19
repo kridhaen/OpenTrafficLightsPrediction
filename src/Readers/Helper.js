720a580aaca70e4aa750f167697efd7ea7cedb91
kridhaen <Kurt.Dhaene@UGent.be>
2019-04-19 17:25:53 +0200

    prefix fix

--- src/Readers/Helper.js
+++ src/Readers/Helper.js
@@ -16,7 +16,7 @@ class Helper{
 
     static writeN3Store(store, prefixes){
         return new Promise(async (resolve) => {
-            const writer = new n3.Writer({ "prefixes": {prefixes}});
+            const writer = new n3.Writer({ "prefixes": prefixes});
             for(let quad of store.getQuads()){
                 writer.addQuad(quad);
             }
