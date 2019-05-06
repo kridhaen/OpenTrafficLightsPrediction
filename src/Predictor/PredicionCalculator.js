d288c044e14e74264a0742cc46ad536777b85aef
kridhaen <Kurt.Dhaene@UGent.be>
2019-05-06 10:26:21 +0200

    median calculator

--- src/Predictor/PredicionCalculator.js
+++ src/Predictor/PredicionCalculator.js
@@ -13,6 +13,30 @@ class PredictionCalculator{
             return Math.round(weightedSum / count); //TODO: kan delen door 0 -> NaN
         }
     }
+
+    static calculateMedianDuration(frequencyDistribution){
+        let count = 0;
+        Object.keys(frequencyDistribution).forEach((duration) => {
+            count+=frequencyDistribution[duration];
+        });
+        let runner = 0;
+        let median = undefined;
+        let i = 0;
+        let list = Object.keys(frequencyDistribution);
+        while(median === undefined && i<list.length){
+            if(count/2 > runner && count/2 <= runner+frequencyDistribution[list[i]]){
+                median = list[i];
+            }
+            runner+=frequencyDistribution[list[i]];
+            i++;
+        }
+        if(count === 0){
+            return undefined;
+        }
+        else {
+            return parseInt(median, 10);
+        }
+    }
 }
 
 module.exports = PredictionCalculator;
--- src/Predictor/_test/PredictionCalculation.test.js
+++ src/Predictor/_test/PredictionCalculation.test.js
@@ -9,3 +9,18 @@ test('PredictionCalculation: calculateMeanDuration empty distribution', () => {
     let distribution = {};
     expect(PredictionCalculator.calculateMeanDuration(distribution)).toBe(undefined);
 });
+
+test('PredictionCalculation: calculateMedianDuration', () => {
+    let distribution = { "10": 5, "20": 10 };
+    expect(PredictionCalculator.calculateMedianDuration(distribution)).toEqual(20);
+});
+
+test('PredictionCalculation: calculateMedianDuration same', () => {
+    let distribution = { "10": 5, "20": 5 };
+    expect(PredictionCalculator.calculateMedianDuration(distribution)).toEqual(10);
+});
+
+test('PredictionCalculation: calculateMedianDuration empty distribution', () => {
+    let distribution = {};
+    expect(PredictionCalculator.calculateMedianDuration(distribution)).toBe(undefined);
+});
