91164dd4544aa8e0b3a6ad4e84ea85e2eefe81be
kridhaen <Kurt.Dhaene@UGent.be>
2019-05-06 18:16:38 +0200

    DurationsManager: store previous durations

---
+++ src/Distributions/DurationsManager.js
@@ -0,0 +1,43 @@
+//store the duration of phases
+class DurationsManager {
+    constructor(historySize){
+        this.durations = {};
+        this.historySize = historySize; //configure the amount of durations needs to be stored for each signalGroup and signalPhase
+    }
+
+    add(signalGroup, signalPhase, duration){
+        if(!this.durations[signalGroup]){
+            this.durations[signalGroup] = {};
+        }
+        if(!this.durations[signalGroup][signalPhase]) {
+            this.durations[signalGroup][signalPhase] = [];
+        }
+        this.durations[signalGroup][signalPhase].push(duration);
+        if(this.durations[signalGroup][signalPhase].length > this.historySize){
+            this.durations[signalGroup][signalPhase].shift();
+        }
+    }
+
+    getLastHistory(signalGroup, signalPhase){
+        if(this.durations[signalGroup]
+            && this.durations[signalGroup][signalPhase]
+        ){
+            return this.durations[signalGroup][signalPhase];
+        }
+    }
+
+    getLastDuration(signalGroup, signalPhase){
+        if(this.durations[signalGroup]
+            && this.durations[signalGroup][signalPhase]
+        ){
+            if(this.durations[signalGroup][signalPhase].length > 0){
+                return this.durations[signalGroup][signalPhase][this.durations[signalGroup][signalPhase].length - 1];
+            }
+        }
+        else{
+            return undefined;
+        }
+    }
+}
+
+module.exports = DurationsManager;
