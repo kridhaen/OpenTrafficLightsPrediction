6ed34b9e135ec7d37c3cb5f66c1a351d209eb1d0
kridhaen <Kurt.Dhaene@UGent.be>
2019-05-14 19:11:56 +0200

    add suite to calculate deviations between multiple runs

---
+++ src/Testing/TestSuite/TestFileReader.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+
+class HistoricFileSystemReader{
+    constructor(folderpath, dataParts, onLearnFile, onTestFile, onBeforeRun, onRunComplete, onAllRunComplete){
+        this.readAndParseSync = this.readAndParseSync.bind(this);
+        this.onLearnFile = onLearnFile;
+        this.onTestFile = onTestFile;
+        this.folderpath = folderpath;
+        this.dataParts = dataParts;
+        this.onRunComplete = onRunComplete;
+        this.onAllRunComplete = onAllRunComplete;
+        this.onBeforeRun = onBeforeRun;
+    }
+
+    readAndParseSync() {
+        return new Promise((resolve) => {
+            let temp = this;
+            console.log("\x1b[31m", "reading", "\x1b[0m");
+            this.files = undefined;
+            fs.readdir(this.folderpath, async function (err, files) {
+                if (err) {
+                    console.log(err);
+                }
+                console.log("\x1b[31m", "read dir", "\x1b[0m");
+
+                let filesPerPart = Math.round(files.length / temp.dataParts);
+                let testPartStart = 0;
+                let testPartEnd = 0;
+                console.log("\x1b[31m", "total files in dir: "+files.length, "\x1b[0m");
+                for(let testRun = 0; testRun < temp.dataParts; testRun++){
+                    console.log("\x1b[31m", "starting run: " + testRun, "\x1b[0m");
+                    temp.onBeforeRun();
+                    testPartStart = testRun * filesPerPart;
+                    testPartEnd = testPartStart + filesPerPart;
+
+                    let i = 0;
+                    let fileRunner = 0;
+                    for (let file of files.sort()) {  //alle files overlopen, gesorteerd volgens naam, wat overeenkomt met de datum, oudste eerst
+                        if(fileRunner < testPartStart || fileRunner > testPartEnd){
+                            if (i++ % 100 === 0) HistoricFileSystemReader.showProgress(i, files.length-filesPerPart, "files");
+                            let data = fs.readFileSync(temp.folderpath + "/" + file);
+                            let fragment = data.toString();
+                            await temp.onLearnFile(fragment, file);
+                        }
+                        fileRunner++;
+                    }
+                    console.log("\x1b[31m", "learning files: "+i, "\x1b[0m");
+                    i = 0;
+                    for (let fileNr = testPartStart; fileNr <= testPartEnd; fileNr++){
+                        if (i++ % 100 === 0) HistoricFileSystemReader.showProgress(i, filesPerPart, "files");
+                        let data = fs.readFileSync(temp.folderpath + "/" + files[fileNr]);
+                        let fragment = data.toString();
+                        await temp.onTestFile(fragment, files[fileNr]);
+                    }
+                    console.log("\x1b[31m", "testing files: "+i, "\x1b[0m");
+
+                    temp.onRunComplete();
+                }
+
+                temp.onAllRunComplete();
+
+                console.log("\x1b[31m", "run complete", "\x1b[0m");
+
+
+                resolve();
+            });
+        });
+
+    }
+
+    static showProgress(current,maximum,description){
+        console.log("Running: "+" "+description+": "+current+"/"+maximum);
+    }
+}
+
+module.exports = HistoricFileSystemReader;
---
+++ src/Testing/TestSuite/main.js
@@ -0,0 +1,78 @@
+const DistributionStore = require('../../Distributions/DistributionStore.js');
+const TestFileReader = require('./TestFileReader.js');
+const PredictionPublisher = require('../../Publisher/PredictionPublisher.js');
+const FragmentParser = require('../../Readers/FragmentParser.js');
+const DistributionManager = require('../../Distributions/DistributionManager.js');
+const DurationsManager = require('../../Distributions/DurationsManager.js');
+const Analytics = require('../../Analytics/Result.js');
+
+//const filepath = "./previous";
+const filepath =  "./previous_small";
+
+let distributionStore = new DistributionStore();
+DistributionManager.createDistributions(distributionStore);
+let durationsManager = new DurationsManager(1);
+
+let analytics = new Analytics(distributionStore, durationsManager);
+let observations = 0;
+let changes = 0;
+let same = 0;
+let clearNoEndYet = 0;
+
+let historicFragmentParser = new FragmentParser();  //TODO: remove file?
+let testHistoricFragmentParser = new FragmentParser();
+let historicFileSystemReader = new TestFileReader(filepath, 10, async (fragment, file) => {
+
+    await historicFragmentParser.handleFragment(fragment, file, (returnObject) => {
+        let { signalGroup, signalPhase, generatedAtTime, lastPhaseStart, lastPhase, minEndTime, maxEndTime } = returnObject;
+        DistributionManager.storeInDistribution(generatedAtTime, lastPhaseStart, signalGroup, lastPhase, distributionStore);    //correct
+        changes++;
+    }, (returnObject) => {
+        same++
+    }, () => {observations++}, undefined, (signalGroup) => {
+
+    });
+},async (fragment, file) => {
+
+    await testHistoricFragmentParser.handleFragment(fragment, file, (returnObject) => {
+        let {signalGroup, signalPhase, generatedAtTime, lastPhaseStart, lastPhase, minEndTime, maxEndTime} = returnObject;
+        analytics.add(undefined, generatedAtTime, signalGroup, signalPhase, lastPhase, minEndTime, maxEndTime, generatedAtTime, generatedAtTime, lastPhaseStart); //TODO: uitwerken
+        durationsManager.add(signalGroup, lastPhase, new Date(generatedAtTime).getTime() / 1000 - new Date(lastPhaseStart).getTime() / 1000);
+        changes++;
+    }, (returnObject) => {
+        let {signalGroup, signalPhase, generatedAtTime, lastPhaseStart, lastPhase, minEndTime, maxEndTime} = returnObject;
+        analytics.add(undefined, lastPhaseStart, signalGroup, signalPhase, lastPhase, minEndTime, maxEndTime, generatedAtTime, undefined, lastPhaseStart);
+        same++
+    }, () => {
+        observations++
+    }, undefined, (signalGroup) => {
+        analytics.clearNoEndYetList(signalGroup); //TODO: clear ook lijst, want als ongeldig kan einde niet meer worden gevonden, want is gereset, dus volgende einde hoort misschien niet bij diegene in de lijst
+        clearNoEndYet++;
+    });
+}, () => {
+    historicFragmentParser = new FragmentParser();
+    testHistoricFragmentParser = new FragmentParser();
+}, () => {
+
+}, () => {
+
+});
+
+let predictionPublisher = new PredictionPublisher(8080);
+predictionPublisher.start();
+historicFileSystemReader.readAndParseSync()
+    .then(() => {
+        let b = process.hrtime(a);
+        console.log(b);
+
+        console.log("same: "+ same+"\nchanges: "+changes+"\nobservations: "+observations+"\nerrors in fragmentParser: "+(observations-changes-same)+"\ncleared NoEndYet: "+clearNoEndYet+"\n");
+        historicFragmentParser.printDebugInfo();
+        console.log("calculate predictions");
+        let analyticsList = analytics.calculate();
+        predictionPublisher.setJSONDistributionEndpoint("analytics", analyticsList);
+        console.log("calculate deviations");
+        let deviations = analytics.showLoss();
+        predictionPublisher.setJSONDistributionEndpoint("deviations", deviations);
+
+
+    });
